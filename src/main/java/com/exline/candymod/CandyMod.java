94e9abd291fc688f19fd6f27a0334c59c1a47586
jexline <joeyexline@gmail.com>
2023-02-23 11:20:17 -0800

    Updated to 1.19.3

--- src/main/java/com/exline/candymod/CandyMod.java
+++ src/main/java/com/exline/candymod/CandyMod.java
@@ -2,18 +2,21 @@ package com.exline.candymod;
 
 import com.exline.candymod.items.ModItems;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class CandyMod implements ModInitializer {
 
     public static final String MOD_ID = "candymod";
 
-    public static final ItemGroup CANDY_GROUP = FabricItemGroupBuilder.create(
-            new Identifier(MOD_ID, "tab")).icon(() ->
-            new ItemStack(ModItems.LOLLIPOP)).build();
+    public static final ItemGroup ITEM_GROUP = FabricItemGroup.builder(
+                    new Identifier(MOD_ID, "tab"))
+            .displayName(Text.literal("Candy"))
+            .icon(() -> new ItemStack(ModItems.LOLLIPOP))
+            .build();
 
     @Override
     public void onInitialize() {
--- src/main/java/com/exline/candymod/items/ModItems.java
+++ src/main/java/com/exline/candymod/items/ModItems.java
@@ -1,26 +1,34 @@
 package com.exline.candymod.items;
 
 import com.exline.candymod.CandyMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.*;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 public class ModItems {
 
     //Foods
-    public static final Item CHOCOLATE_BAR = new Item(new Item.Settings().group(CandyMod.CANDY_GROUP).food(new FoodComponent.Builder().hunger(3).saturationModifier(0.3F).snack().build()));
-    public static final Item JELLY_BEANS = new Item(new Item.Settings().group(CandyMod.CANDY_GROUP).food(new FoodComponent.Builder().hunger(2).saturationModifier(0.2F).snack().build()));
-    public static final Item LOLLIPOP = new Item(new Item.Settings().group(CandyMod.CANDY_GROUP).food(new FoodComponent.Builder().hunger(4).saturationModifier(0.4F).snack().build()));
-    public static final Item COTTON_CANDY = new Item(new Item.Settings().group(CandyMod.CANDY_GROUP).food(new FoodComponent.Builder().hunger(6).saturationModifier(0.6F).snack().build()));
-    public static final Item CANDY_CANE = new Item(new Item.Settings().group(CandyMod.CANDY_GROUP).food(new FoodComponent.Builder().hunger(4).saturationModifier(0.4F).snack().build()));
-    public static final Item TAFFY = new Item(new Item.Settings().group(CandyMod.CANDY_GROUP).food(new FoodComponent.Builder().hunger(4).saturationModifier(0.4F).snack().build()));
+    public static final Item CHOCOLATE_BAR = new Item(new Item.Settings().food(new FoodComponent.Builder().hunger(3).saturationModifier(0.3F).snack().build()));
+    public static final Item JELLY_BEANS = new Item(new Item.Settings().food(new FoodComponent.Builder().hunger(2).saturationModifier(0.2F).snack().build()));
+    public static final Item LOLLIPOP = new Item(new Item.Settings().food(new FoodComponent.Builder().hunger(4).saturationModifier(0.4F).snack().build()));
+    public static final Item COTTON_CANDY = new Item(new Item.Settings().food(new FoodComponent.Builder().hunger(6).saturationModifier(0.6F).snack().build()));
+    public static final Item CANDY_CANE = new Item(new Item.Settings().food(new FoodComponent.Builder().hunger(4).saturationModifier(0.4F).snack().build()));
+    public static final Item TAFFY = new Item(new Item.Settings().food(new FoodComponent.Builder().hunger(4).saturationModifier(0.4F).snack().build()));
 
     public static void registerItems() {
-        Registry.register(Registry.ITEM, new Identifier(CandyMod.MOD_ID, "chocolate_bar"), CHOCOLATE_BAR);
-        Registry.register(Registry.ITEM, new Identifier(CandyMod.MOD_ID, "jelly_beans"), JELLY_BEANS);
-        Registry.register(Registry.ITEM, new Identifier(CandyMod.MOD_ID, "lollipop"), LOLLIPOP);
-        Registry.register(Registry.ITEM, new Identifier(CandyMod.MOD_ID, "cotton_candy"), COTTON_CANDY);
-        Registry.register(Registry.ITEM, new Identifier(CandyMod.MOD_ID, "candy_cane"), CANDY_CANE);
-        Registry.register(Registry.ITEM, new Identifier(CandyMod.MOD_ID, "taffy"), TAFFY);
+        registerItem("chocolate_bar", CHOCOLATE_BAR);
+        registerItem("jelly_beans", JELLY_BEANS);
+        registerItem("lollipop", LOLLIPOP);
+        registerItem("cotton_candy", COTTON_CANDY);
+        registerItem("candy_cane", CANDY_CANE);
+        registerItem("taffy", TAFFY);
+    }
+
+    public static void registerItem(String name, Item item) {
+        Registry.register(Registries.ITEM, new Identifier(CandyMod.MOD_ID, name), item);
+        ItemGroupEvents.modifyEntriesEvent(CandyMod.ITEM_GROUP).register(content -> {
+            content.add(item);});
     }
 }
